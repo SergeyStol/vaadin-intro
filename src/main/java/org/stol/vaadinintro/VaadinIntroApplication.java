da7cd11941fbaa8bdc6d73b57e2f6d1358310a63
Sergey123456 <SergeyStol123@gmail.com>
2021-06-05 21:16:33 +0300

    Update ui using another thread (manual push)

--- src/main/java/org/stol/vaadinintro/VaadinIntroApplication.java
+++ src/main/java/org/stol/vaadinintro/VaadinIntroApplication.java
@@ -2,8 +2,10 @@ package org.stol.vaadinintro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VaadinIntroApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/stol/vaadinintro/views/MainView.java
+++ src/main/java/org/stol/vaadinintro/views/MainView.java
@@ -1,29 +1,50 @@
 package org.stol.vaadinintro.views;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.shared.communication.PushMode;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Route("")
 @Theme(variant = Lumo.DARK)
+@Push(PushMode.MANUAL)
 public class MainView extends VerticalLayout {
+
+    Label label = new Label();
+
     public MainView() {
         TextField textField = new TextField("Please, enter something and push the button");
         Button button = new Button("Push me");
         AtomicInteger counter = new AtomicInteger();
+        label.setText("I am a label");
         button.addClickListener(event -> {
             Notification.show("You've entered: " + textField.getValue());
             Notification.show("Number of clicks: " + counter.incrementAndGet(),
                     5000, Notification.Position.BOTTOM_END);
             Notification.show("Number of clicks(from event): " + event.getClickCount(),
                     5000, Notification.Position.BOTTOM_END);
+            label.setText(String.valueOf(counter.get()));
         });
-        add(textField, button);
+        add(textField, button, label);
+    }
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 1000)
+    public void changeLabelValue() {
+//        label.setText(String.valueOf(0));  It doesn't work
+        getUI().ifPresent(ui -> {
+            ui.access(() -> {
+                label.setText(String.valueOf(0));
+                ui.push();
+            });
+        });
     }
 }
